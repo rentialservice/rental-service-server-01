335e8a1c228100836172073d225cf1b2bb2b8469
Kashif Sheikh <keenkashif13@gmail.com>
2024-10-29 00:53:03 +0530

    every linkage done

--- src/modules/firm/firm.module.ts
+++ src/modules/firm/firm.module.ts
@@ -4,9 +4,10 @@ import { FirmService } from './firm.service';
 import { FirmController } from './firm.controller';
 import { Firm } from './entities/firm.entity';
 import { SubscriptionModule } from '../subscription/subscription.module';
+import { CategoryModule } from '../category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Firm]), SubscriptionModule],
+  imports: [TypeOrmModule.forFeature([Firm]), SubscriptionModule, CategoryModule],
   providers: [FirmService],
   controllers: [FirmController],
   exports: [FirmService],
