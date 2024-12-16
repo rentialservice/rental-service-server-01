d38354505e0a6f999c493611969ba5cc40feabdd
Kashif Sheikh <keenkashif13@gmail.com>
2024-12-16 22:51:47 +0530

    removed buyers entity

--- src/modules/users/buyer/entities/buyer.entity.ts
+++ src/modules/users/buyer/entities/buyer.entity.ts
@@ -2,7 +2,6 @@ import { BaseEntity } from '../../../../base/base.entity';
 import { Entity, Column, OneToMany, ManyToOne } from 'typeorm';
 import { NotificationToken } from '../../../supporting-modules/notification/entities/notification-token.entity';
 import { Firm } from '../../../firm/entities/firm.entity';
-import { Role } from '../../../role/entities/role.entity';
 import { Rental } from '../../../rental/entities/rental.entity';
 
 @Entity("buyer")
@@ -48,39 +47,6 @@ export class Buyer extends BaseEntity {
 
   @ManyToOne(() => Firm)
   firm: Firm;
-
-  @ManyToOne(() => Role)
-  role: Role;
-
-  @Column({
-    default:
-      'https://res.cloudinary.com/twitter-clone-media/image/upload/v1597737557/user_wt3nrc.png',
-  })
-  avatar: string;
-
-  @Column({
-    default:
-      'https://images.unsplash.com/photo-1462332420958-a05d1e002413?q=80&w=2107&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  })
-  cover: string;
-
-  @Column({ default: '' })
-  bio: string;
-
-  @Column({ default: '' })
-  dob: string;
-
-  @Column({ default: 'offline' })
-  status: string;
-
-  @Column('boolean', { default: false })
-  verified: boolean;
-
-  @Column('boolean', { default: false })
-  ssoLogin: boolean;
-
-  @Column({ type: 'timestamptz', default: new Date() })
-  lastSeen: Date;
 
   @OneToMany(() => NotificationToken, (notificationToken) => notificationToken.user)
   notificationTokens: NotificationToken[];
