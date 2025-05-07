ae8255bf0720b63051e13ffc1b42e4a745ac1a01
kashif <keenkashif13@gmail.com>
2025-05-08 01:22:07 +0530

    fix

--- src/modules/auth/jwt.auth.guard.ts
+++ src/modules/auth/jwt.auth.guard.ts
@@ -3,35 +3,38 @@ import {
   ExecutionContext,
   Injectable,
   UnauthorizedException,
-} from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { JwtService } from '@nestjs/jwt';
-import { IS_PUBLIC_KEY } from './public.decorator';
+} from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { JwtService } from "@nestjs/jwt";
+import { IS_PUBLIC_KEY } from "./public.decorator";
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
-    private reflector: Reflector,
+    private reflector: Reflector
   ) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(
-      IS_PUBLIC_KEY,
-      [context.getHandler(), context.getClass()],
-    );
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
     if (isPublic) return true;
 
     const request = context.switchToHttp().getRequest();
-    const token = request.headers.authorization?.split(' ')[1];
-    if (!token) throw new UnauthorizedException('Token missing');
+    console.log({ request });
+    const token = request.headers.authorization?.split(" ")[1];
+    console.log({ token });
+    if (!token) throw new UnauthorizedException("Token missing");
 
     try {
       const payload = this.jwtService.verify(token);
       request.user = payload.user || payload;
       return true;
-    } catch {
-      throw new UnauthorizedException('Invalid token');
+    } catch (error) {
+      console.log({ error });
+      throw new UnauthorizedException("Invalid token");
     }
   }
 }
